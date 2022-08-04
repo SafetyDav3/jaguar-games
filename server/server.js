87c40e6b0228c2b3968e446fac6d41dd9ec52482
Alan Edlebeck <alan.edlebeck@gmail.com>
2022-08-04 18:22:34 -0500

    added server variable for heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const {ApolloServer} = require('apollo-server-express')
 const {typeDefs, resolvers} = require('./schema')
 const {authMiddleware} = require('./utils/auth')
 require('dotenv').config()
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 
 const app = express()
 
